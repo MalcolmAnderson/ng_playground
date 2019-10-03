bb8710d25ed969bb1081bdf0b129ecec93bba9eb
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-03 09:10:23 -0700

    now select it as a class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  selector: '.app-servers',
   // selector: 'app-servers',
-  selector: '[app-servers]',
-//  templateUrl: './servers.component.html',
+  // selector: '[app-servers]',
+  // templateUrl: './servers.component.html',
   template: `
   <app-server></app-server>
   <app-server></app-server>
