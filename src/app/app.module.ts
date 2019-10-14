9929d8361883a29df57007992cb4105bfbdea48e
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-14 09:22:19 -0700

    control and databinding work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ResetUserNameComponent } from './reset-user-name/reset-user-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    ResetUserNameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/reset-user-name/reset-user-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-user-name',
+  templateUrl: './reset-user-name.component.html',
+  styleUrls: ['./reset-user-name.component.css']
+})
+export class ResetUserNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
